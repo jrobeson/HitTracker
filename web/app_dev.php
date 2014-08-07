91e045957653c2797004238bea8870c5d2f20465
Johnny Robeson <johnny@localmomentum.net>
2014-08-07 05:59:21 -0400

    remove class caching for dev

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,13 +12,12 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     exit('You are not allowed to access this file.');
 }
 
-$loader = require_once __DIR__.'/../var/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
