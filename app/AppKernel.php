37e880123b510bfbde7cb4da26b0ccf5fe3efca8
Johnny Robeson <johnny@localmomentum.net>
2014-08-14 07:41:21 -0400

    add kunstmann/live-reload-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
+            $bundles[] = new Kunstmaan\LiveReloadBundle\KunstmaanLiveReloadBundle();
             $bundles[] = new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
