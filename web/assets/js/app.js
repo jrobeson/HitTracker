946f0f93207200948fc435a6ead5a4332d509317
Johnny Robeson <johnny@localmomentum.net>
2014-09-03 16:00:31 -0400

    update compiled app.js

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -13296,6 +13296,47 @@ colors = jQuery.Color.names = {
         return true;
     }
 
+    function buildForm(action, method, csrfToken, params) {
+        var form      = document.createElement('form');
+
+        form.method   = 'POST';
+        form.action   = action;
+
+        var input     = document.createElement('input');
+
+        input.type    = 'hidden';
+        input.name    = '_method';
+        input.value   = method;
+
+        form.appendChild(input);
+
+        if (csrfToken) {
+            var csrfInput = document.createElement('input');
+            csrfInput.type  = 'hidden';
+            csrfInput.name  = '_link_token';
+            csrfInput.value = csrfToken;
+            form.appendChild(csrfInput);
+        }
+
+        if (params) {
+            var extraFields = {};
+            var parts = params.split('&');
+            for (var i = 0, len = parts.length; i < len; i++) {
+                var tokens = parts[i].split('=');
+                extraFields[tokens[0]] = decodeURIComponent(tokens[1]);
+            }
+            for (var key in extraFields) {
+                var field = document.createElement('input');
+                field.type = 'hidden';
+                field.name = key;
+                field.value = extraFields[key];
+                form.appendChild(field);
+            }
+        }
+
+        return form;
+    }
+
     $(document).ready(function() {
         $('body').delegate('a[data-method]', 'click', function (event) {
             event.preventDefault();
@@ -13304,29 +13345,13 @@ colors = jQuery.Color.names = {
                 return;
             }
 
-            var form      = document.createElement('form');
-            var input     = document.createElement('input');
-            var csrfInput = document.createElement('input');
             var csrfToken = $(event.currentTarget).data('csrf-token');
+            var action   = event.currentTarget.href;
+            var method   = $(event.currentTarget).data('method');
+            var params   = $(event.currentTarget).data('params');
 
-            form.method   = 'POST';
-            form.action   = event.currentTarget.href;
-
-            input.type    = 'hidden';
-            input.name    = '_method';
-            input.value   = $(event.currentTarget).data('method');
-
-            form.appendChild(input);
-
-            if (csrfToken) {
-                csrfInput.type  = 'hidden';
-                csrfInput.name  = '_link_token';
-                csrfInput.value = csrfToken;
-                form.appendChild(csrfInput);
-            }
-
+            var form = buildForm(action, method, csrfToken, params);
             document.body.appendChild(form);
-
             form.submit();
         });
     });
