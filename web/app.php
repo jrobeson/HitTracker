c02b654f0b33f9a69933baeba335fabbbfb4fc58
Johnny Robeson <johnny@localmomentum.net>
2014-09-28 17:27:58 -0400

    move app_dev.php code to app.php

--- web/app.php
+++ web/app.php
@@ -1,24 +1,39 @@
 <?php
-
-use Symfony\Component\ClassLoader\ApcClassLoader;
+/**
+ * Symfony App Front Controller
+ *
+ * @todo consider using the ApcClassLoader
+ */
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../var/bootstrap.php.cache';
+$env = 'prod';
+$debug = false;
 
-/*
-$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
+// choose environment
+if ('app_dev.php' == basename($_SERVER['SCRIPT_FILENAME'])) {
+    $env = 'dev';
+}
 
+
+// use environment
+if ('dev' == $env) {
+    $debug = true;
+    require_once __DIR__.'/../app/autoload.php';
+    if (isset($_SERVER['HTTP_CLIENT_IP'])
+        || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+        || !(in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1'])
+        || php_sapi_name() === 'cli-server')
+    ) {
+        header('HTTP/1.0 403 Forbidden');
+        exit('You are not allowed to access this file.');
+    }
+} else {
+    require_once __DIR__.'/../var/bootstrap.php.cache';
+}
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
-//$kernel = new AppCache($kernel);
+$kernel = new AppKernel($env, $debug);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
--- web/app_dev.php
+++ web/app_dev.php
@@ -1,28 +1,2 @@
 <?php
-use Symfony\Component\HttpFoundation\Request;
-
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
-    || php_sapi_name() === 'cli-server')
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file.');
-}
-
-$loader = require_once __DIR__.'/../app/autoload.php';
-
-require_once __DIR__.'/../app/AppKernel.php';
-
-$kernel = new AppKernel('dev', true);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-
-if (function_exists('register_postsend_function')) {
-    register_postsend_function(function () use ($kernel, $request, $response) {
-        $kernel->terminate($request, $response);
-    });
-} else {
-    $kernel->terminate($request, $response);
-}
+include 'app.php';
