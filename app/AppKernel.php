2ab40daf7db26d7183595f45fdf02396b155d024
Johnny Robeson <johnny@localmomentum.net>
2014-11-24 18:11:33 -0500

    remove ladybug-bundle in favor of debugbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -50,7 +50,6 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Kunstmaan\LiveReloadBundle\KunstmaanLiveReloadBundle();
-            $bundles[] = new RaulFraile\Bundle\LadybugBundle\RaulFraileLadybugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
         }
