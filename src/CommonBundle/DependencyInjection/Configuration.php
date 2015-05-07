b4c22c14b35b88aaf2a5c0e996527dcbb997ecae
Johnny Robeson <johnny@localmomentum.net>
2015-05-07 02:07:00 -0400

    remove unused $rootNode from CommonBundle Configuration class

--- src/CommonBundle/DependencyInjection/Configuration.php
+++ src/CommonBundle/DependencyInjection/Configuration.php
@@ -5,18 +5,11 @@ namespace LazerBall\HitTracker\CommonBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * {@inheritdoc}
- */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritdoc}
-     */
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('hittracker_common');
 
         return $treeBuilder;
     }
