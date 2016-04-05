6b790771b1b926f2f45a1ac79051cb2e51c82a5e
Johnny Robeson <johnny@localmomentum.net>
2016-04-05 19:16:01 -0400

    add help text to site settings

--- src/GameBundle/Settings/SiteSettingsSchema.php
+++ src/GameBundle/Settings/SiteSettingsSchema.php
@@ -47,27 +47,51 @@ class SiteSettingsSchema implements SchemaInterface
             ->add('arenas', 'integer', [
                 'label' => 'hittracker.settings.site.arenas',
                 'constraints' => [new Assert\GreaterThan(['value' => 0])],
+                'attr' => [
+                    'data-help' => 'hittracker.settings.site.arenas.help',
+                ],
             ])
             ->add('business_name', 'text', [
                 'label' => 'hittracker.settings.site.business_name',
+                'attr' => [
+                    'data-help' => 'hittracker.settings.site.business_name.help',
+                ],
             ])
             ->add('business_address', 'textarea', [
                 'label' => 'hittracker.settings.site.business_address',
+                'attr' => [
+                    'data-help' => 'hittracker.settings.site.business_address.help',
+                ],
             ])
             ->add('business_phone', 'text', [
                 'label' => 'hittracker.settings.site.business_phone',
+                'attr' => [
+                    'data-help' => 'hittracker.settings.site.business_phone.help',
+                ],
             ])
             ->add('business_email', 'text', [
                 'label' => 'hittracker.settings.site.business_email',
+                'attr' => [
+                    'data-help' => 'hittracker.settings.site.business_email.help',
+                ],
             ])
             ->add('scoreboard_logo', 'text', [
                 'label' => 'hittracker.settings.site.scoreboard_logo',
+                'attr' => [
+                    'data-help' => 'hittracker.settings.site.scoreboard_logo.help',
+                ],
             ])
             ->add('scoreboard_banner_1', 'text', [
                 'label' => 'hittracker.settings.site.scoreboard_banner_1',
+                'attr' => [
+                    'data-help' => 'hittracker.settings.site.scoreboard_banner_1.help',
+                ],
             ])
             ->add('scoreboard_banner_2', 'text', [
                 'label' => 'hittracker.settings.site.scoreboard_banner_2',
+                'attr' => [
+                    'data-help' => 'hittracker.settings.site.scoreboard_banner_2.help',
+                ],
             ])
         ;
     }
