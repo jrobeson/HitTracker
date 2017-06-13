39bb19acbe8e7bbd6a866e3e0cb5f2c6655661bf
Johnny Robeson <johnny@localmomentum.net>
2017-06-13 04:33:01 -0400

    remove CommonBundle

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -61,7 +61,6 @@ class AppKernel extends Kernel
             new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new \FOS\RestBundle\FOSRestBundle(),
             new \JMS\SerializerBundle\JMSSerializerBundle(),
-            new \LazerBall\HitTracker\CommonBundle\CommonBundle(),
             new \LazerBall\HitTracker\PubSubBundle\PubSubBundle(),
             new \LazerBall\HitTracker\GameBundle\HitTrackerGameBundle(),
             new \C33s\StaticPageContentBundle\C33sStaticPageContentBundle(),
