8602b2eec00f408b16bc29c5a29be50ff843648b
johnny <johnny@localmomentum.net>
2017-09-04 08:49:10 -0400

    fix url in toggleVest

--- app/Resources/assets/js/app.js
+++ app/Resources/assets/js/app.js
@@ -25,7 +25,7 @@ import { alertDismiss } from './ui-util'
 
 const toggleVest = (address, value) => {
     let request = $.ajax({
-        url: `http://localhost:3000/games/${address}/${value}`,
+        url: `http://localhost:3000/unit/${address}/${value}`,
         headers: {
             Accept: 'application/json'
         }
