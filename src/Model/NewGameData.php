ec938e3e5b1f299f343d81817d10ae31c790b6e0
johnny <johnny@localmomentum.net>
2017-09-15 17:41:35 -0400

    remove unused Collection import

--- src/Model/NewGameData.php
+++ src/Model/NewGameData.php
@@ -19,7 +19,6 @@
 namespace LazerBall\HitTracker\Model;
 
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use LazerBall\HitTracker\Validator\Constraints as CommonAssert;
 use Symfony\Component\Validator\Constraints as Assert;
 
