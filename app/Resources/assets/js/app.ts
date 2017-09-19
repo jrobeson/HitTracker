57b4857dcf5f6f3cce3bc81369b0091de192f339
johnny <johnny@localmomentum.net>
2017-09-19 02:28:33 -0400

    reformatting js files

--- app/Resources/assets/js/app.ts
+++ app/Resources/assets/js/app.ts
@@ -25,142 +25,153 @@ import './jquery.color.js';
 import { alertDismiss, printScores } from './ui-util';
 
 const toggleVest = (address: string, value: number) => {
-    const request = $.ajax({
-        url: `${window.location.origin}/blegateway/unit/${address}/${value}`,
-        type: 'POST',
-        headers: {
-            Accept: 'application/json'
-        }
-    });
-    request.done(() => true);
+  const request = $.ajax({
+    url: `${window.location.origin}/blegateway/unit/${address}/${value}`,
+    type: 'POST',
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  request.done(() => true);
 };
 
 $(document).ready(() => {
-    alertDismiss();
+  alertDismiss();
 
-    $('.unit-turn-on').click(function (e) {
-        const address = $(this).data('unit-address');
-        toggleVest(address, 1);
-        e.preventDefault();
+  $('.unit-turn-on').click(function(e) {
+    const address = $(this).data('unit-address');
+    toggleVest(address, 1);
+    e.preventDefault();
+  });
+  $('.unit-turn-off').click(function(e) {
+    const address = $(this).data('unit-address');
+    toggleVest(address, 0);
+    e.preventDefault();
+  });
+
+  $('form[id="game_form"] select[id$="_unit"]').focusout(function(e) {
+    const address = $(this)
+      .children('option:selected')
+      .data('unit-address');
+    if (address) {
+      toggleVest(address, 1);
+    }
+  });
+  $('form[id="game_form"] select[id="game_reload_players"]').change(function() {
+    const gameId = $(this).val();
+    const request = $.ajax({
+      url: `${window.location.origin}/games/${gameId}`,
+      headers: {
+        Accept: 'application/json',
+      },
     });
-    $('.unit-turn-off').click(function (e) {
-        const address = $(this).data('unit-address');
-        toggleVest(address, 0);
-        e.preventDefault();
-    });
-
-    $('form[id="game_form"] select[id$="_unit"]').focusout(function (e) {
-        const address = $(this).children('option:selected').data('unit-address');
-        if (address) {
-            toggleVest(address, 1);
-        }
-    });
-    $('form[id="game_form"] select[id="game_reload_players"]').change(function () {
-        const gameId = $(this).val();
-        const request = $.ajax({
-            url: `${window.location.origin}/games/${gameId}`,
-            headers: {
-                Accept: 'application/json'
-            }
-        });
-        request.done((game) => {
-            const teamPlayers = {} as any;
-            for (const player of game.players) {
-                const pt = player.team;
-                if (!teamPlayers.hasOwnProperty(pt)) {
-                    teamPlayers[pt] = {};
-                }
-
-                teamPlayers[pt][player.unit.id] = player;
-            }
-
-            const teams = Object.keys(teamPlayers);
-
-            $('.new-game-teams').each(function () {
-                const team = teams.shift() || '';
-                $(this).find('.team-no input').val(team);
-                const players = teamPlayers[team];
-                const playerList = Object.values(players) as any[];
-                $(this).find('.player').each(function() {
-                    const thisPlayer = playerList.shift() as any;
-                    let name = '';
-                    let unitId = '';
-                    if (thisPlayer) {
-                        name = thisPlayer.name;
-                        unitId = thisPlayer.unit.id;
-                    }
-                    $(this).find('input[id$="_name"]').val(name);
-                    const unitSelector = $(this).find('select[id$="_unit"]');
-                    unitSelector.val(unitId);
-                    unitSelector.trigger('focusout');
-                });
-            });
-        });
-    });
-
-    $('form[name="game"]').submit(function () {
-        $('.new-game-teams').each(function () {
-            const team = $(this).find('.team-no input').val() as string;
-            $(this).find('input[id$="_team"]').each(function () {
-                $(this).val(team.trim());
-            });
-        });
-    });
-
-    if ($('body').hasClass('hittracker-game-active') || $('body').hasClass('hittracker-game-scoreboard')) {
-        const source = new EventSource('/events/game');
-        $(window).on('unload',() => {
-                source.close();
-        });
-
-        if ($('body').hasClass('hittracker-game-scoreboard')) {
-            source.addEventListener('game.start', (e: any) => {
-                window.location.reload(true);
-            });
+    request.done(game => {
+      const teamPlayers = {} as any;
+      for (const player of game.players) {
+        const pt = player.team;
+        if (!teamPlayers.hasOwnProperty(pt)) {
+          teamPlayers[pt] = {};
         }
 
-        source.addEventListener('game.end', (e: any) => {
-            queueActivity('<li>Game Ends</li>');
+        teamPlayers[pt][player.unit.id] = player;
+      }
+
+      const teams = Object.keys(teamPlayers);
+
+      $('.new-game-teams').each(function() {
+        const team = teams.shift() || '';
+        $(this)
+          .find('.team-no input')
+          .val(team);
+        const players = teamPlayers[team];
+        const playerList = Object.values(players) as any[];
+        $(this)
+          .find('.player')
+          .each(function() {
+            const thisPlayer = playerList.shift() as any;
+            let name = '';
+            let unitId = '';
+            if (thisPlayer) {
+              name = thisPlayer.name;
+              unitId = thisPlayer.unit.id;
+            }
+            $(this)
+              .find('input[id$="_name"]')
+              .val(name);
+            const unitSelector = $(this).find('select[id$="_unit"]');
+            unitSelector.val(unitId);
+            unitSelector.trigger('focusout');
+          });
+      });
+    });
+  });
+
+  $('form[name="game"]').submit(function() {
+    $('.new-game-teams').each(function() {
+      const team = $(this)
+        .find('.team-no input')
+        .val() as string;
+      $(this)
+        .find('input[id$="_team"]')
+        .each(function() {
+          $(this).val(team.trim());
         });
+    });
+  });
 
-        source.addEventListener('game.hit', (e: any) => {
-            const eventData = $.parseJSON(e.data);
-            const hit = eventData.content;
-            const targetPlayer = hit.target_player;
+  if ($('body').hasClass('hittracker-game-active') || $('body').hasClass('hittracker-game-scoreboard')) {
+    const source = new EventSource('/events/game');
+    $(window).on('unload', () => {
+      source.close();
+    });
 
-            // queueActivity(`<li>${player.name} hit $[targetPlayer.name} in Zone ${targetPlayer.zone}</li>`);
-            if (targetPlayer.zone_hits) {
-                pushHit(`.player-${targetPlayer.id} .zone-${targetPlayer.zone}`, parseInt(targetPlayer.zone_hits, 10));
-            }
-            if (targetPlayer.hit_points) {
-                $(`.player-${targetPlayer.id} .player-hit-points`).text(targetPlayer.hit_points);
-                const team = targetPlayer.team.replace(' ', '-').toLowerCase();
-                $(`.${team} .team-total-hp`).text(hit.target_team_hit_points);
-            }
-            if (targetPlayer.score) {
-                $(`.player-${targetPlayer.id} .player-score`).text(targetPlayer.score);
-                const team = targetPlayer.team.replace(' ', '-').toLowerCase();
-                $(`.${team} .team-total-score`).text(hit.target_team_score);
-            }
-
-        });
-
-        initCountdown($('#game-time-countdown'));
+    if ($('body').hasClass('hittracker-game-scoreboard')) {
+      source.addEventListener('game.start', (e: any) => {
+        window.location.reload(true);
+      });
     }
 
-    $('#print-scores').click(function (event) {
-        event.preventDefault();
-        const scoreElement = $(this) as any;
-        printScores(scoreElement.attr('href'));
+    source.addEventListener('game.end', (e: any) => {
+      queueActivity('<li>Game Ends</li>');
     });
+
+    source.addEventListener('game.hit', (e: any) => {
+      const eventData = $.parseJSON(e.data);
+      const hit = eventData.content;
+      const targetPlayer = hit.target_player;
+
+      // queueActivity(`<li>${player.name} hit $[targetPlayer.name} in Zone ${targetPlayer.zone}</li>`);
+      if (targetPlayer.zone_hits) {
+        pushHit(`.player-${targetPlayer.id} .zone-${targetPlayer.zone}`, parseInt(targetPlayer.zone_hits, 10));
+      }
+      if (targetPlayer.hit_points) {
+        $(`.player-${targetPlayer.id} .player-hit-points`).text(targetPlayer.hit_points);
+        const team = targetPlayer.team.replace(' ', '-').toLowerCase();
+        $(`.${team} .team-total-hp`).text(hit.target_team_hit_points);
+      }
+      if (targetPlayer.score) {
+        $(`.player-${targetPlayer.id} .player-score`).text(targetPlayer.score);
+        const team = targetPlayer.team.replace(' ', '-').toLowerCase();
+        $(`.${team} .team-total-score`).text(hit.target_team_score);
+      }
+    });
+
+    initCountdown($('#game-time-countdown'));
+  }
+
+  $('#print-scores').click(function(event) {
+    event.preventDefault();
+    const scoreElement = $(this) as any;
+    printScores(scoreElement.attr('href'));
+  });
 });
 
 function queueActivity(content: any) {
-    const gameActivitySelector = $('.game-activity ul li') as any;
-    if (gameActivitySelector.size() > 10) {
-        $('.game-activity ul li:first').remove();
-    }
-    $('.game-activity ul').append(content);
+  const gameActivitySelector = $('.game-activity ul li') as any;
+  if (gameActivitySelector.size() > 10) {
+    $('.game-activity ul li:first').remove();
+  }
+  $('.game-activity ul').append(content);
 }
 
 /**
@@ -172,35 +183,38 @@ function queueActivity(content: any) {
  * @param Object selector a jquery selector for the target element
  */
 function initCountdown(selector: any) {
-    let gameEnd = selector.data('game-end-time');
-    const clientTime = (new Date()).getTime();
-    const serverTime = new Date(parseInt(selector.data('server-time'), 10)).getTime();
-    const offset = serverTime - clientTime;
-    gameEnd = gameEnd - offset;
+  let gameEnd = selector.data('game-end-time');
+  const clientTime = new Date().getTime();
+  const serverTime = new Date(parseInt(selector.data('server-time'), 10)).getTime();
+  const offset = serverTime - clientTime;
+  gameEnd = gameEnd - offset;
 
-    const formatDate = (event: any) => {
-        let format = '%M:%S';
-        if (event.offset.hours > 0) {
-            format = `%-H:${format}`;
-        }
-        return event.strftime(format);
-    };
-    selector.countdown(gameEnd)
-        .on('update.countdown', function (event: any) {
-            $(event.target).text(formatDate(event));
-        }).on('finish.countdown', function(event: any) {
-            $(event.target).text(formatDate(event));
-            const musicSelector = $('#active-game-music') as any;
-            if (musicSelector.length) {
-			    musicSelector.get(0).pause();
-		    }
-        });
+  const formatDate = (event: any) => {
+    let format = '%M:%S';
+    if (event.offset.hours > 0) {
+      format = `%-H:${format}`;
+    }
+    return event.strftime(format);
+  };
+  selector
+    .countdown(gameEnd)
+    .on('update.countdown', function(event: any) {
+      $(event.target).text(formatDate(event));
+    })
+    .on('finish.countdown', function(event: any) {
+      $(event.target).text(formatDate(event));
+      const musicSelector = $('#active-game-music') as any;
+      if (musicSelector.length) {
+        musicSelector.get(0).pause();
+      }
+    });
 }
 
 function pushHit(selector: any, zoneHits: number) {
-    const value = $(selector).text();
-    $(selector).animate({color: '#a50b00'}, 500, function () {
-        $(this).text(zoneHits);
-    }).animate({color: '#000'}, 500);
+  const value = $(selector).text();
+  $(selector)
+    .animate({ color: '#a50b00' }, 500, function() {
+      $(this).text(zoneHits);
+    })
+    .animate({ color: '#000' }, 500);
 }
-
--- app/Resources/assets/js/ui-util.ts
+++ app/Resources/assets/js/ui-util.ts
@@ -1,62 +1,66 @@
 export const alertDismiss = () => {
-    const target = $('.alert');
-    let timeout = target.data('auto-dismiss');
+  const target = $('.alert');
+  let timeout = target.data('auto-dismiss');
 
-    if (!timeout) {
-        return;
-    }
-    timeout = parseInt(timeout, 10) * 1000;
-    setTimeout(() => {
-        target.fadeTo(500, 0).slideUp(500, function() {
-            $(this).remove();
-        });
-    }, timeout);
+  if (!timeout) {
+    return;
+  }
+  timeout = parseInt(timeout, 10) * 1000;
+  setTimeout(() => {
+    target.fadeTo(500, 0).slideUp(500, function() {
+      $(this).remove();
+    });
+  }, timeout);
 };
 
 export const printScores = (url: string) => {
-    const frame = document.createElement('iframe');
-    frame.setAttribute('id', 'print-frame');
-    frame.setAttribute('name', 'print-frame');
-    frame.setAttribute('type', 'content');
-    frame.setAttribute('collapsed', 'true');
-    document.documentElement.appendChild(frame);
+  const frame = document.createElement('iframe');
+  frame.setAttribute('id', 'print-frame');
+  frame.setAttribute('name', 'print-frame');
+  frame.setAttribute('type', 'content');
+  frame.setAttribute('collapsed', 'true');
+  document.documentElement.appendChild(frame);
 
-    frame.addEventListener('load', (event) => {
-        jsPrintSetup.clearSilentPrint();
-        jsPrintSetup.setPaperSizeUnit(jsPrintSetup.kPaperSizeInches);
-        const paperSizeId = 200;
-        jsPrintSetup.definePaperSize(
-            paperSizeId,
-            paperSizeId,
-            'lazerball_scorecard',
-            'lazerball_scorecard_8.5x5.5in',
-            'LazerBall Scorecard',
-            8.5,
-            5.5,
-            jsPrintSetup.kPaperSizeInches
-        );
-        jsPrintSetup.setPaperSizeData(1);
+  frame.addEventListener(
+    'load',
+    event => {
+      jsPrintSetup.clearSilentPrint();
+      jsPrintSetup.setPaperSizeUnit(jsPrintSetup.kPaperSizeInches);
+      const paperSizeId = 200;
+      jsPrintSetup.definePaperSize(
+        paperSizeId,
+        paperSizeId,
+        'lazerball_scorecard',
+        'lazerball_scorecard_8.5x5.5in',
+        'LazerBall Scorecard',
+        8.5,
+        5.5,
+        jsPrintSetup.kPaperSizeInches
+      );
+      jsPrintSetup.setPaperSizeData(1);
 
-        jsPrintSetup.setOption('orientation', jsPrintSetup.kPortraitOrientation);
-        jsPrintSetup.setOption('shrinkToFit', true);
-        jsPrintSetup.setOption('marginTop', 0);
-        jsPrintSetup.setOption('marginBottom', 0);
-        jsPrintSetup.setOption('marginLeft', 0);
-        jsPrintSetup.setOption('marginRight', 0);
-        jsPrintSetup.setOption('headerStrLeft', '');
-        jsPrintSetup.setOption('headerStrCenter', '');
-        jsPrintSetup.setOption('headerStrRight', '');
-        jsPrintSetup.setOption('footerStrLeft', '');
-        jsPrintSetup.setOption('footerStrCenter', '');
-        jsPrintSetup.setOption('footerStrRight', '');
-        jsPrintSetup.printWindow(frame.contentWindow);
+      jsPrintSetup.setOption('orientation', jsPrintSetup.kPortraitOrientation);
+      jsPrintSetup.setOption('shrinkToFit', true);
+      jsPrintSetup.setOption('marginTop', 0);
+      jsPrintSetup.setOption('marginBottom', 0);
+      jsPrintSetup.setOption('marginLeft', 0);
+      jsPrintSetup.setOption('marginRight', 0);
+      jsPrintSetup.setOption('headerStrLeft', '');
+      jsPrintSetup.setOption('headerStrCenter', '');
+      jsPrintSetup.setOption('headerStrRight', '');
+      jsPrintSetup.setOption('footerStrLeft', '');
+      jsPrintSetup.setOption('footerStrCenter', '');
+      jsPrintSetup.setOption('footerStrRight', '');
+      jsPrintSetup.printWindow(frame.contentWindow);
 
-        setTimeout(() => {
-            if (frame) {
-                frame.remove();
-            }
-        }, 10);
-    }, true);
+      setTimeout(() => {
+        if (frame) {
+          frame.remove();
+        }
+      }, 10);
+    },
+    true
+  );
 
-    frame.contentDocument.location.href = url;
+  frame.contentDocument.location.href = url;
 };
