5f0941560719701f830617275d625d42595f9096
johnny <johnny@localmomentum.net>
2018-05-24 01:15:39 -0400

    remove migrations dir from PackageCommand

--- src/Command/PackageCommand.php
+++ src/Command/PackageCommand.php
@@ -120,7 +120,7 @@ class PackageCommand extends Command
 
     private function copyFiles(string $sourceDir, string $targetDir, bool $useExistingVendor = false): void
     {
-        $appDirs = ['bin', 'etc', 'migrations', 'src', 'public', 'translations', 'templates'];
+        $appDirs = ['bin', 'etc', 'src', 'public', 'translations', 'templates'];
         if ($useExistingVendor) {
             $appDirs[] = 'vendor';
         }
