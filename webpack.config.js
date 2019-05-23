50b22bace0722f09e9b20dcf1f44181fa2da386f
Johnny Robeson <johnny@localmomentum.net>
2019-05-23 05:17:42 -0400

    reformat webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,50 @@
 const path = require('path');
 const Encore = require('@symfony/webpack-encore');
 
+Encore.setOutputPath('public/build/')
+  .setPublicPath('/build')
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .addEntry('js/app', './assets/js/app.ts')
 
-Encore
-    .setOutputPath('public/build/')
-    .setPublicPath('/build')
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-    .addEntry('js/app', './assets/js/app.ts')
+  //.addStyleEntry('global', './assets/styles/global.scss')
+  .addStyleEntry('style/app', ['./assets/style/app.scss'])
+  .addStyleEntry('style/scoreboard', ['./assets/style/scoreboard.scss'])
+  .addStyleEntry('style/scorecard', ['./assets/style/scorecard.scss'])
 
-    //.addStyleEntry('global', './assets/styles/global.scss')
-    .addStyleEntry('style/app', ['./assets/style/app.scss'])
-    .addStyleEntry('style/scoreboard', ['./assets/style/scoreboard.scss'])
-    .addStyleEntry('style/scorecard', ['./assets/style/scorecard.scss'])
+  .configureBabel(babelConfig => {
+    babelConfig.presets.push('es2017');
+  })
+  .enableSassLoader(
+    sassOptions => {
+      sassOptions.precision = 10;
+    },
+    {
+      resolveUrlLoader: false,
+    }
+  )
+  .enableTypeScriptLoader(tsConfig => {})
+  .enableForkedTypeScriptTypesChecking(tsForkedConfig => {
+    tsForkedConfig.vue = true;
+    tsForkedConfig.tslint = true;
+  })
+  .enablePostCssLoader()
+  .enableVueLoader()
 
-    .configureBabel((babelConfig) => {
-        babelConfig.presets.push('es2017');
-    })
-    .enableSassLoader((sassOptions) => {
-        sassOptions.precision = 10;
-    }, {
-        resolveUrlLoader: false
-    })
-    .enableTypeScriptLoader((tsConfig) => {
-    })
-    .enableForkedTypeScriptTypesChecking((tsForkedConfig) => {
-        tsForkedConfig.vue = true;
-        tsForkedConfig.tslint = true;
-    })
-    .enablePostCssLoader()
-    .enableVueLoader()
+  .autoProvideVariables({
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+  })
 
-    .autoProvideVariables({
-        '$': 'jquery',
-        'jQuery': 'jquery',
-        'window.jQuery': 'jquery'
-    })
-
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
-;
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction());
 
 const config = Encore.getWebpackConfig();
 config.resolve.alias = {
-    ...config.resolve.alias,
-    '@': path.resolve(__dirname, './assets/js'),
-    'style': path.resolve(__dirname, './assets/style'),
+  ...config.resolve.alias,
+  '@': path.resolve(__dirname, './assets/js'),
+  style: path.resolve(__dirname, './assets/style'),
 };
 
 module.exports = config;
