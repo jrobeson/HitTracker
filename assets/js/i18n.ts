c64e47374a7f28924cfe2ed2871401a83fa0a7d1
Johnny Robeson <johnny@localmomentum.net>
2019-07-31 22:35:35 -0400

    disable i18n debugging

--- assets/js/i18n.ts
+++ assets/js/i18n.ts
@@ -9,7 +9,7 @@ i18n
     resources,
     fallbackLng: 'en',
     lng: 'en',
-    debug: true,
+    debug: false,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
